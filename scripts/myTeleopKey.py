ffcb2ebb080506324b26a302981a0781d02382b1
Jhonbrando <75914747+Jhonbrando@users.noreply.github.com>
2022-04-03 21:18:13 -0500

    Update myTeleopKey.py

--- scripts/myTeleopKey.py
+++ scripts/myTeleopKey.py
@@ -1,3 +1,7 @@
 #Archivo para generar el nuevo TeleopKey
 
 import rospy
+from geometry_msgs import Twist
+from turtlesim.srv import TeleportAbsolute, TeleportRelative
+import termios, sys, os
+from numpy import pi
